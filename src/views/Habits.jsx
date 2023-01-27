e410d366ca681466acc03a9425ea7be840bb8798
Marcin Skic <marcinskicoficjal@gmail.com>
2023-01-27 14:55:15 +0100

    Test habit index() request

--- src/views/Habits.jsx
+++ src/views/Habits.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import axiosClient from "../axios-client";
 import CreateHabit from "./CreateHabit";
 
 export default function Habits(){
+
+    useEffect(() => {
+        axiosClient.post('/habits')
+        .then((response)=>{
+            console.log(response);
+        })
+        .catch((error) => {
+
+            console.log(error.response.data.errors); 
+        })
+    });
+
     return (
         <>
             <div>
